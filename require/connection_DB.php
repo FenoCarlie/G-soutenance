a981872a628c1cbc1e8fb92f29ee681cc9131404
FENO Carlie <95887231+FenoCarlie@users.noreply.github.com>
2023-03-22 11:31:25 +0300

    update

--- require/connection_DB.php
+++ require/connection_DB.php
@@ -2,7 +2,7 @@
 $hostname = "localhost";
 $username = "root";
 $password = "";
-$database = "gestion_des_soutenances1";
+$database = "gestion_des_soutenances";
 $conn = mysqli_connect($hostname, $username, $password, $database);
 if (mysqli_connect_errno()) {
     die("La connexion a échoué : " . mysqli_connect_error());
